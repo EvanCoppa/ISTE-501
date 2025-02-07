2b6291f18fd878b6dd2933d19d5b6787115a7f8a
Evan Coppa <42395557+EvanCoppa@users.noreply.github.com>
2025-02-07 18:43:24 -0500

    add server API for data fetching and update routes to display data

--- client/src/routes/todo/+page.svelte
+++ client/src/routes/todo/+page.svelte
@@ -6,14 +6,9 @@ function goForward() {
 }
 </script>
 
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<div >
     <button on:click={goForward}>Increment</button>
     {go}
-</body>
-</html>
+
+</div>
+  
