cd306b4fa8c8e9eb2c3fc6e90ace7472927add29
Evan Coppa <42395557+EvanCoppa@users.noreply.github.com>
2025-02-07 18:32:46 -0500

    added todo

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,6 +1,10 @@
-<script>
-	import '../app.css';
+<script lang="ts">
 	let { children } = $props();
 </script>
+
+<nav>
+	<a href="/">home</a>
+	<a href="/todo">todo</a>
+</nav>
 
 {@render children()}
