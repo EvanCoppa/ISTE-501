6d84f0c7169cc9283a845d347ab3bd2044fb6255
Evan Coppa <42395557+EvanCoppa@users.noreply.github.com>
2025-02-07 18:44:12 -0500

    Reapply "add new routes and server API for data fetching"
    This reverts commit 69115326e0289bc9ae4ff2c45c6d7820305658dd.

--- client/src/routes/todo/+page.svelte
+++ client/src/routes/todo/+page.svelte
@@ -6,9 +6,14 @@ function goForward() {
 }
 </script>
 
-<div >
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
     <button on:click={goForward}>Increment</button>
     {go}
-
-</div>
-  
+</body>
+</html>
