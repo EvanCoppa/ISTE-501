9a4a218e65a9935aad1acdbd792f0b3f9565daf7
Evan Coppa <42395557+EvanCoppa@users.noreply.github.com>
2025-02-15 01:59:40 -0500

    Update welcome message in +page.svelte

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit, Evan</h1>
+<h1>Welcome to SvelteKit, Evan helloasdfasfsdaf</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
